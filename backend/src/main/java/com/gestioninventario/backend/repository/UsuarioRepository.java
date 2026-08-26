6dec85dd1bc7d88c09e8ad8b2549bac78918b0df
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-08-26 16:04:31 -0600

    Creacion de repositorio JPA para Usuarios

---
+++ backend/src/main/java/com/gestioninventario/backend/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.gestioninventario.backend.repository;
+
+import com.gestioninventario.backend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
