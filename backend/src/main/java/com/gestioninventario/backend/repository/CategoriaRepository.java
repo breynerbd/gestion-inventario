d842a75ceca76d222b902f2554e1591186876671
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-08-26 16:03:33 -0600

    Creacion de repositorio JPA para categorias

---
+++ backend/src/main/java/com/gestioninventario/backend/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.gestioninventario.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gestioninventario.backend.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+}
