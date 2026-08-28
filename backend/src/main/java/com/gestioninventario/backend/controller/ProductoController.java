119321be10a4d0bae58d3e477c0463d0bbeb68ef
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-08-28 11:16:20 -0600

    Correccion de anotacion mal escrita. @Controller a @RestController

--- backend/src/main/java/com/gestioninventario/backend/controller/ProductoController.java
+++ backend/src/main/java/com/gestioninventario/backend/controller/ProductoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.gestioninventario.backend.entity.Producto;
 import com.gestioninventario.backend.service.ProductoService;
 
-@Controller
+@RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
 
